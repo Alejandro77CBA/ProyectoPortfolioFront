5851a3348bc5715a86f1a87743459347808d1491
Alejandro77CBA <alejandrovibares27@gmail.com>
2022-10-15 10:54:01 -0300

    avances4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,8 @@ import { ExperienciaComponent } from './components/experiencia/experiencia.compo
 import { EducacionComponent } from './components/educacion/educacion.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
 import { HySComponent } from './components/hy-s/hy-s.component';
+import { ProyectoComponent } from './components/proyecto/proyecto.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -23,7 +25,9 @@ import { HySComponent } from './components/hy-s/hy-s.component';
     AcercaDeComponent,
     ExperienciaComponent,
     EducacionComponent,
-    HySComponent
+    HySComponent,
+    ProyectoComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
